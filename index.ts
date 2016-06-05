1d0b9d350adba11b1bd830799642561edbbdd38f
Emma Kuo <emma.c.kuo@gmail.com>
2016-06-05 13:18:07 -0700

    got it working

--- index.ts
+++ index.ts
@@ -3,11 +3,28 @@ import http = require('http');
 import jade = require('jade');
 import mfo = require('mf-obj');
 var debug = require('debug')('thready');
+import util = require('./util');
 
 var app = express();
 app.disable('x-powered-by');
 app.set('views', './template');
 app.set('view engine', 'jade');
+app.use(express.static('static', {extensions: ['html']}));
+
+app.get('/', async (req, res) => {
+    try {
+        if (req.query.url) {
+            var thread = await mfo.getThreadFromUrl(req.query.url);
+            thread.sort(mfo.Entry.byDate);
+            res.render('threadpage', {thread:thread, util: util});
+        } else {
+            res.render('querypage');
+        }        
+    } catch (err) {
+        debug('Server error: ' + err);
+        res.sendStatus(500);
+    }
+});
 
 var server = http.createServer(app);
 
