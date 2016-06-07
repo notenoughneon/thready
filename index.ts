251e28120ab0ca76c1bd4b69cec4a089312fdac1
Emma Kuo <emma.c.kuo@gmail.com>
2016-06-07 15:56:47 -0700

    log requests

--- index.ts
+++ index.ts
@@ -14,6 +14,7 @@ app.use(express.static('static', {extensions: ['html']}));
 app.get('/', async (req, res) => {
     try {
         if (req.query.url) {
+            debug('%s %s', req.ip, req.query.url);
             var thread = await mfo.getThreadFromUrl(req.query.url, {strategies: ['entry', 'event']}, false);
             thread = thread.filter(e => !e.isLike() && !e.isRepost());
             thread.sort(mfo.Entry.byDate);
